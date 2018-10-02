29a8f30f69981787fc3f9e9a69e52f52cfa32229
teddymkekana <teddy@sov.tech>
2018-10-02 23:29:43 +0200

    Update readme

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Router } from "react-router-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+import { Provider } from 'react-redux';
+import Amplify from "aws-amplify";
+import cognito from "./cognito";
+import store from './store';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import * as serviceWorker from './serviceWorker';
+const history = createBrowserHistory();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: cognito.cognito.REGION,
+    userPoolId: cognito.cognito.USER_POOL_ID,
+    identityPoolId: cognito.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: cognito.cognito.APP_CLIENT_ID
+  }
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+        <App />
+    </Router>
+  </Provider>, document.querySelector('.container'));
 serviceWorker.unregister();
